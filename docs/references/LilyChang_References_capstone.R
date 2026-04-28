7110066ccba156deae09e62e3418fd89f35c53c1
Lily Chang <chang.lily.m@gmail.com>
2026-04-27 20:29:40 -0400

    Add files via upload

---
+++ docs/references/LilyChang_References_capstone.R
@@ -0,0 +1,43 @@
+References
+
+
+- Negative binomial regression model references: 
+  
+https://youtu.be/lUbtXI3XdPE?si=YmB2RdJ25i7422p8 
+
+https://dplyr.tidyverse.org/reference/summarise.html
+
+
+- Cleaning references:
+
+https://www.nbcwashington.com/news/local/noisy-for-profit-parties-at-private-homes-face-higher-fines-in-montgomery-co/3957191/ 
+
+https://stackoverflow.com/questions/48754775/select-command-not-working-in-r-even-after-installing-the-library-dplyr 
+
+https://stackoverflow.com/questions/71413141/set-2-digits-in-month 
+
+https://www.statology.org/replace-na-with-string-in-r/#:~:text=by%20Zach%20Bobbitt%20Published%20on%20June%202%2C,%25%3E%25%20replace_na(list(x%20=%20'missing'%2C%20y%20=%20'none')) 
+
+https://stackoverflow.com/questions/45101670/how-do-i-replace-na-values-with-specific-values-in-an-r 
+  
+https://stackoverflow.com/questions/1435435/javascript-regular-expression-to-match-5-or-9-digit-zipcode  
+
+
+- ARIMA references:
+  
+https://www.youtube.com/watch?v=VPhyVSJMbpA
+
+
+- EDA references:
+  
+https://data.montgomerycountymd.gov/Technology/Geographic-data-Zip-Codes-Shape-File-/vz4m-d8ee
+
+https://data.montgomerycountymd.gov/Technology/Geographic-data-Zip-Codes-Shape-File-/vz4m-d8ee
+
+
+
+
+
+
+
+
